dafd0a746f7f72fb5a1c6fe23893aaa2ea2766f9
politinizer <113183482+likemindsforpeterobi@users.noreply.github.com>
2023-05-01 11:14:32 +0100

    Authenticated Component in LoginForm

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,20 +1,28 @@
+import { useLocation, Navigate } from 'react-router-dom';
 import TableHead from './TableHead';
 import TableBody from './TableBody';
 import useOpenSky from '../hooks/useOpenSky';
 
 const Dashboard = () => {
    const users = useOpenSky();
-   console.log(users);
-
-   return (
-      <>
-         <h3>Open Sky Dashboard</h3>
-         <table className='table'>
-            <TableHead />
-            <TableBody users={users} />
-         </table>
-      </>
+   const location = useLocation();
+   const authenticated = new URLSearchParams(location.search).get(
+      'authenticated',
    );
+
+   if (authenticated === 'true') {
+      return (
+         <>
+            <h3>Open Sky Dashboard</h3>
+            <table className='table'>
+               <TableHead />
+               <TableBody users={users} />
+            </table>
+         </>
+      );
+   } else {
+      return <Navigate to='/' />;
+   }
 };
 
 export default Dashboard;
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
 const LoginForm = () => {
+   const [emailAndPassword, setEmailAndPassword] = useState({
+      email: '',
+      password: '',
+   });
+   const [formError, setFormError] = useState('');
+
    let navigate = useNavigate();
 
    const {
       register,
       handleSubmit,
-      formState: { errors },
+      formState: { errors, isValid },
    } = useForm();
 
    const onSubmit = (data) => {
@@ -18,72 +25,101 @@ const LoginForm = () => {
 
       if (email === emailstored && password === passStored) {
          console.log('login successful');
-         navigate('/dashboard');
+         navigate('/dashboard?authenticated=true');
       } else {
-         alert('Invalid credentials');
+         setFormError('Invalid credentials');
+      }
+   };
+
+   const handleRegister = () => {
+      // handle register button click
+      const emailStored = localStorage.getItem('email');
+      const passStored = localStorage.getItem('password');
+
+      if (!emailStored || !passStored) {
+         localStorage.setItem('email', 'admin@admin.com');
+         localStorage.setItem('password', 'admin');
+         setEmailAndPassword({ email: 'admin@admin.com', password: 'admin' });
       }
    };
 
    return (
-      <div className='d-flex align-items-center justify-content-center vh-100 bg-secondary'>
-         <form
-            onSubmit={handleSubmit(onSubmit)}
-            className='w-25 border p-3 rounded bg-light'
-         >
-            <div className='mb-3'>
-               <label htmlFor='email' className='form-label text-bg-success'>
-                  Email address
-               </label>
-               <input
-                  {...register('email', {
-                     required: true,
-                     minLength: 3,
-                     pattern:
-                        /^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
-                  })}
-                  type='text'
-                  className='form-control'
-                  id='email'
-               />
-               {errors.email?.type === 'required' && (
-                  <p className='text-danger'>Email is required</p>
-               )}
-               {errors.email?.type === 'minLength' && (
-                  <p className='text-danger'>
-                     Minimum must have more than 3 characters
-                  </p>
-               )}
-               {errors.email?.type === 'pattern' && (
-                  <p className='text-danger'>Must be a valid Email</p>
-               )}
+      <>
+         <div className='d-flex align-items-center justify-content-center vh-100 bg-secondary'>
+            <div className='w-25 border p-3 rounded bg-light'>
+               <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className='mb-3'>
+                     <label
+                        htmlFor='email'
+                        className='form-label text-bg-success'
+                     >
+                        Email address
+                     </label>
+                     <input
+                        {...register('email', {
+                           required: true,
+                           minLength: 3,
+                           pattern:
+                              /^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
+                        })}
+                        type='text'
+                        className='form-control'
+                        id='email'
+                     />
+                     {errors.email?.type === 'required' && (
+                        <p className='text-danger'>Email is required</p>
+                     )}
+                     {errors.email?.type === 'minLength' && (
+                        <p className='text-danger'>
+                           Minimum must have more than 3 characters
+                        </p>
+                     )}
+                     {errors.email?.type === 'pattern' && (
+                        <p className='text-danger'>Must be a valid Email</p>
+                     )}
+                  </div>
+                  <div className='mb-3'>
+                     <label
+                        htmlFor='password'
+                        className='form-label text-bg-success'
+                     >
+                        Password
+                     </label>
+                     <input
+                        {...register('password', {
+                           required: true,
+                           minLength: 4,
+                        })}
+                        type='password'
+                        className='form-control'
+                        id='password'
+                     />
+                     {errors.password?.type === 'required' && (
+                        <p className='text-danger'>Password is required</p>
+                     )}
+                     {errors.password?.type === 'minLength' && (
+                        <p className='text-danger'>
+                           Password must be more than 3 characters
+                        </p>
+                     )}
+                  </div>
+                  <button
+                     disabled={!isValid}
+                     type='submit'
+                     className='btn btn-primary float-end'
+                  >
+                     Submit
+                  </button>
+                  <p className='text-danger'>{formError}</p>
+               </form>
+               <div className='d-flex align-items-center float-end w-100 '>
+                  <button className='btn btn-link ' onClick={handleRegister}>
+                     Register here to login...
+                  </button>
+               </div>
             </div>
-            <div className='mb-3'>
-               <label htmlFor='password' className='form-label text-bg-success'>
-                  Password
-               </label>
-               <input
-                  {...register('password', {
-                     required: true,
-                     minLength: 4,
-                  })}
-                  type='password'
-                  className='form-control'
-                  id='password'
-               />
-               {errors.password?.type === 'required' && (
-                  <p className='text-danger'>Password is required</p>
-               )}
-               {errors.password?.type === 'minLength' && (
-                  <p className='text-danger'>
-                     Password must be more than 3 characters
-                  </p>
-               )}
-            </div>
-            <button type='submit' className='btn btn-primary float-end'>
-               Submit
-            </button>
-         </form>
-      </div>
+         </div>
+      </>
    );
 };
 
