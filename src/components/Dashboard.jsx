5a91e4055b4bde7acd9d4adb7ca9b7563cf6df03
politinizer <113183482+likemindsforpeterobi@users.noreply.github.com>
2023-05-01 12:03:35 +0100

    Done before extracting Email Input

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useLocation, Navigate } from 'react-router-dom';
+import { useLocation, Navigate, useNavigate } from 'react-router-dom';
 import TableHead from './TableHead';
 import TableBody from './TableBody';
 import useOpenSky from '../hooks/useOpenSky';
@@ -6,14 +6,30 @@ import useOpenSky from '../hooks/useOpenSky';
 const Dashboard = () => {
    const users = useOpenSky();
    const location = useLocation();
+   const navigate = useNavigate();
+
    const authenticated = new URLSearchParams(location.search).get(
       'authenticated',
    );
 
+   const handleLogout = () => {
+      localStorage.clear();
+      navigate('/');
+   };
+
    if (authenticated === 'true') {
       return (
          <>
-            <h3>Open Sky Dashboard</h3>
+            <div className='d-flex mb-3'>
+               <div className='p-2 fs-3'>Dashboard</div>
+               <button
+                  className='btn btn-link ms-auto p-2'
+                  onClick={handleLogout}
+               >
+                  Logout
+               </button>
+            </div>
+
             <table className='table'>
                <TableHead />
                <TableBody users={users} />
